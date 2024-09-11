5e19493e3cb774c68409aea5340e08d5b3961672
ProjectNiki <nikidwijananto@gmail.com>
2024-09-12 02:10:07 +0700

    testing tanpa extension

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,7 +6,7 @@
                 <h2 class="mb-2 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
             </a>
             <div class="text-base text-gray-500">
-                <a href="#">{{ $post['author'] }}</a> | 3 Januari 2024
+                <a href="#">{{ $post['author'] }}</a> | 10 Januari 2024
             </div>
             <p class="my-4 font-light">{{ Str::limit($post['body'], 200) }}</p>
             <a href="/posts/{{ $post['id'] }}" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
